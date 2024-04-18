3694f973a34a020132140f414c374436e496e93c
VINAY C N <VINAY C N@LAPTOP-VVQO476E>
2024-04-18 17:37:32 +0530

    new commit

--- TrelloFrameWork/src/test/java/activeelement/NewDemo1.java
+++ TrelloFrameWork/src/test/java/activeelement/NewDemo1.java
@@ -5,6 +5,9 @@ public class NewDemo1 {
 	public static void main(String[] args) {
 
 		System.out.println("new code ");
+		System.out.println("new code1 ");
+		System.out.println("new code2 ");
+		//
 	}
 
 }
